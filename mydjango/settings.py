2ce2957b88f553e83cb4591332678c64dc1128a2
jaeyo <jaeyongish@gmail.com>
2022-04-19 11:53:21 +0900

    postman연습

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '80sdimflyb9m#f0f&#alqa@$-j^$ee_cs2^%j)ih)jphazizbb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
